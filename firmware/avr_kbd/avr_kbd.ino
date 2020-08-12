225a97723a0bf75c458c71fd2824c6797e3a9b16
Andy Karpov <andy.karpov@gmail.com>
2020-08-12 14:16:08 +0300

    Added link to the SegaController library

--- firmware/avr_kbd/avr_kbd.ino
+++ firmware/avr_kbd/avr_kbd.ino
@@ -3,6 +3,8 @@
  * 
  * Designed to build on Arduino IDE.
  * 
+ * External libraries: https://github.com/jonthysell/SegaController/
+ * 
  * @author Andy Karpov <andy.karpov@gmail.com>
  * Ukraine, 2020
  */
@@ -33,7 +35,7 @@
 #define LED_SPECIAL A3
 
 #if JOYTYPE==JOY_SEGA
-  #include <SegaController.h>
+  #include <SegaController.h> // https://github.com/jonthysell/SegaController/
   SegaController joystick(4, 5, 6, 7, 8, 9, 3); // db9_pin_7, db9_pin_1, db9_pin_2, db9_pin_3, db9_pin_4, db9_pin_6, db9_pin_9
   word joy_current_state = 0;
   word joy_last_state = 0;
